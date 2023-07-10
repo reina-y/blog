c6313d5d3b5013a67e9bde8fb916eab9cd0b3556
reina-y <yamayama017moto@gmail.com>
2023-07-10 15:02:14 +0900

    ワイヤーフレーム

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+ <html>
+    <head>
+        <meta charset="utf-8">
+        <title>blog</title>
+    </head>
+      <body>
+          <h1>blog</h1>
+          <div class='posts'>
+              @foreach($posts as $post)  
+                <div class='post'>
+                   <h2 class='title'>{{$post->title}}</h2>
+                   <p>{{$post->body}}</p>
+              @endforeach
+                 </div>
+      </body>
+ </html>
