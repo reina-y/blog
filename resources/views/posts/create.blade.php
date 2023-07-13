3c59ecce34aaf513c4b111dfc92b05f90c0b77a3
reina-y <yamayama017moto@gmail.com>
2023-07-13 22:59:04 +0900

    保存処理

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,11 +13,10 @@
                </div>
                <div class="body">
                   <p>本文 
-                  <br><textarea name='body' cols=82 rows=40></textarea></p>
+                  <br><textarea name='post[body]' cols=82 rows=20></textarea></p>
                </div>
-           </form>       
-                  <button>保存</button>
-                     <a href='/'>戻る</a>
+               <input type="submit" value="保存"/>
             </form>
+            <a href='/'>戻る</a>
        </body>
 </html>
