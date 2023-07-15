4e0d1cfad7bee94681d49e0955fcef6ba48d3e94
reina-y <yamayama017moto@gmail.com>
2023-07-15 21:17:39 +0900

    編集保存処理

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,15 +6,15 @@
     </head>
     <body>
         <h1>編集</h1>
-        <form action='/posts' method='POST'>
+        <form action='/posts/{{$post->id}}' method='POST'>
             @csrf
+            @method('PUT')
             <div class='title'>
                 <input type='text' name='post[title]' placeholder='タイトル' value="{{$post->title}}{{ old('post.title') }}"/>
             </div>
             <div class='body'>
                 <textarea name='post[body]' placeholder='本文'>{{$post->body}}{{ old('post.body') }}</textarea>
             </div>
-            <input type='submit' value='保存'>
-        </form>
+            <input type='submit' value='保存'/>
         <a href='/'>戻る</a>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ use App\Http\Controllers\PostController;
 
 Route::get('/',[PostController::class,'index']);
 Route::get('/posts/create',[PostController::class,'create']);
-Route::get('/posts/{post}/edit',[PostController::class,'edit']);
 Route::get('/posts/{post}',[PostController::class,'show']);
+Route::get('/posts/{post}/edit',[PostController::class,'edit']);
+Route::put('/posts/{post}',[PostController::class,'update']);
 Route::post('/posts',[PostController::class,'store']);
 
