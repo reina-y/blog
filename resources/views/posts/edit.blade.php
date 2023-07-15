7b2356ab2516c4d3e16f7f865d66aacdfe4ab841
reina-y <yamayama017moto@gmail.com>
2023-07-15 17:42:52 +0900

    投稿編集画面

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset='utf-8'>
+        <title>blog</title>
+    </head>
+    <body>
+        <h1>編集</h1>
+        <form action='/posts' method='POST'>
+            @csrf
+            <div class='title'>
+                <input type='text' name='post[title]' placeholder='タイトル' value="{{$post->title}}{{ old('post.title') }}"/>
+            </div>
+            <div class='body'>
+                <textarea name='post[body]' placeholder='本文'>{{$post->body}}{{ old('post.body') }}</textarea>
+            </div>
+            <input type='submit' value='保存'>
+        </form>
+        <a href='/'>戻る</a>
+    </body>
