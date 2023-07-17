a116658656e4776006f345161b0b9c7e0cdc4da7
reina-y <yamayama017moto@gmail.com>
2023-07-17 15:42:37 +0900

    入力チェック処理

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -10,10 +10,12 @@
             @csrf
             @method('PUT')
             <div class='title'>
-                <input type='text' name='post[title]' placeholder='タイトル' value="{{$post->title}}{{ old('post.title') }}"/>
-            </div>
+                <input type='text' name='post[title]' placeholder='タイトル' value="{{old('post.title', $post->title)}}">
+                <P class='title_error'>{{$errors->first('post.title')}}</P>
+            </div>　
             <div class='body'>
-                <textarea name='post[body]' placeholder='本文'>{{$post->body}}{{ old('post.body') }}</textarea>
+                <textarea name='post[body]' placeholder='本文'>{{old('post.body', $post->body)}}</textarea>
+                <p class'body_error'>{{$errors->first('post.body')}}</p>
             </div>
             <input type='submit' value='保存'/>
         <a href='/'>戻る</a>
